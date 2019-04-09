5f0067112f61bd951aa112046b8c11d122ff4e38
Jonathan <jonathan@macbook-pro-de-jonathan.home>
2019-04-09 23:40:14 +0200

    chore: remove vue-apollo-router from this project

--- packages/@gvs/vue-apollo-router/src/lib/error-handler.js
+++
@@ -1,8 +0,0 @@
-export default function errorHandler(error, { Vue, errorPage }) {
-  Vue.prototype.$routeError = error;
-
-  return {
-    title: "500 Error (Internal Server Error)",
-    component: errorPage
-  };
-}
--- packages/@gvs/vue-apollo-router/src/lib/index.js
+++
@@ -1,5 +0,0 @@
-import VueApolloRouter from "./VueApolloRouter";
-
-export { default as RouterView } from "@/components/RouterView";
-export { default as mergeGraphQLTags } from "./merge-graphql-tags";
-export default VueApolloRouter;
